a3e2723d9432293b9004678fc5d604f3e0cb4a8b
Atae Edrissi <ataeedrissi@gmail.com>
2020-08-15 16:44:19 +0100

    Move models folder

---
+++ models/greenhouseModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const greenhouseSchema = new mongoose.Schema({
+  grower: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  plants: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Plant',
+    },
+  ],
+  ratingsAverage: {
+    type: Number,
+    default: 5,
+    set: (val) => Math.round(val * 10) / 10,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  soil: {
+    type: {
+      type: String,
+    },
+    temperature: {
+      type: Number,
+    },
+    ph: {
+      type: Number,
+    },
+    water_food: String,
+  },
+});
+
+const Greenhouse = mongoose.model('Greenhouse', greenhouseSchema);
+
+module.exports = Greenhouse;
