ede00c9eebbf660150c432472306da562f8b398a
Atae Edrissi <ataeedrissi@gmail.com>
2020-08-19 18:58:30 +0100

    Add plants as virtual field

--- models/greenhouseModel.js
+++ models/greenhouseModel.js
@@ -1,37 +1,49 @@
 const mongoose = require('mongoose');
 
-const greenhouseSchema = new mongoose.Schema({
-  grower: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-  },
-  plants: [
-    {
+const greenhouseSchema = new mongoose.Schema(
+  {
+    grower: {
       type: mongoose.Schema.ObjectId,
-      ref: 'Plant',
+      ref: 'User',
     },
-  ],
-  ratingsAverage: {
-    type: Number,
-    default: 5,
-    set: (val) => Math.round(val * 10) / 10,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  soil: {
-    type: {
-      type: String,
-    },
-    temperature: {
+    // plants: [
+    //   {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'Plant',
+    //   },
+    // ], this will be a virtual field when a grower want to create a plant use /ap1/v1/plants (POST)
+    ratingsAverage: {
       type: Number,
+      default: 5,
+      set: (val) => Math.round(val * 10) / 10,
     },
-    ph: {
+    ratingsQuantity: {
       type: Number,
+      default: 0,
     },
-    water_food: String,
+    soil: {
+      type: {
+        type: String,
+      },
+      temperature: {
+        type: Number,
+      },
+      ph: {
+        type: Number,
+      },
+      water_food: String,
+    },
   },
+  {
+    JSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+greenhouseSchema.virtual('plants', {
+  ref: 'Plant',
+  foreignField: 'grower',
+  localField: '_id',
 });
 
 const Greenhouse = mongoose.model('Greenhouse', greenhouseSchema);
