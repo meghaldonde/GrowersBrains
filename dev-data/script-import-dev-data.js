cc2f4463ef2ae88d9667c36c8a30b1b2e79157d1
Atae Edrissi <ataeedrissi@gmail.com>
2020-08-19 19:15:03 +0100

    Update import/delete data

--- dev-data/script-import-dev-data.js
+++ dev-data/script-import-dev-data.js
@@ -13,6 +13,10 @@ const ReviewGreenhouse = require('../models/reviewGreenhouseModel');
 const ReviewArticle = require('../models/reviewArticleModel');
 const Job = require('../models/jobModel');
 
+//Use this if you will use a local database
+// mongoose.connect('mongodb://localhost:27017/GrowersBrains')
+// .then(() => console.log('Successfully connected to the database!'));
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
